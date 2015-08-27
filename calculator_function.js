21a7f2a7fdd0d6b2f103b6e7a5626b70aebdd113
Niuuuuu <Wuted.cpen@gmail.com>
2015-08-26 18:35:36 -0700

    basic calculator setup

---
+++ calculator_function.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+	var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        } 
+    };
+	var number = "";
+    var newnumber = "";
+    var operator = "";
+    var totaldiv = $("#total");
+    totaldiv.text("0");
+    var numOfDecs = 0;
+    
+    $("#decimal").click(function(){
+        var x;
+        
+        for( x in number ){
+            
+            if(number[x] === "."){
+                numOfDecs++;
+                }
+   
+            } 
+        
+        if(numOfDecs === 0){
+        
+        number += $(this).text();
+		totaldiv.text(number);
+		testNumLength(number); 
+            
+            }
+    
+        
+    });
+    
+    $("#numbers a").not("#clear,#clearall").click(function(){
+		number += $(this).text();
+		totaldiv.text(number);
+		testNumLength(number);
+    });
+    $("#operators a, #side a").not("#equals,#decimal").click(function(){
+		if ($(this).attr("id") === "sqrt"){
+		    operator = "sqrt";
+		    $("#equals").click();
+		    
+		    return;
+		    }
+		operator = $(this).text();
+		newnumber = number;
+		number = "";
+		totaldiv.text("0");
+    });
+    $("#clear,#clearall").click(function(){
+		number = "";
+		totaldiv.text("0");
+		if ($(this).attr("id") === "clearall") {
+			newnumber = "";
+		}
+    });
+    //Add your last .click() here!
+    $("#equals").click(function(){
+    	if (operator === "+"){
+    		number = (parseFloat(number, 10) + parseFloat(newnumber,10)).toString(10);
+    	} else if (operator === "-"){
+    		number = (parseFloat(newnumber, 10) - parseFloat(number,10)).toString(10);
+    	} else if (operator === "÷"){
+    		number = (parseFloat(newnumber, 10) / parseFloat(number,10)).toString(10);
+    	} else if (operator === "×"){
+    		number = (parseFloat(newnumber, 10) * parseFloat(number,10)).toString(10);
+    	} else if (operator === "sqrt"){
+    		number = ( Math.sqrt(parseFloat(number,10))).toString(10); 
+       }  else if (operator === "^"){
+    		number = (Math.pow(parseFloat(newnumber, 10),parseFloat(number,10))).toString(10);
+    	}
+    	
+    	totaldiv.text(number);
+    	testNumLength(number);
+    	number = "";
+    	newnumber = "";
+    });
+});
